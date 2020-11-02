0da7d79a7027d10bc4fb13fc92f172dabacf0383
AlienNT <33194513+AlienNT@users.noreply.github.com>
2020-11-02 02:13:07 +0200

    slider2 v1.0

---
+++ slider2/js/main.js
@@ -0,0 +1,66 @@
+let arrowPrev = document.getElementsByClassName('arrow-prev'),
+    arrowNext = document.getElementsByClassName('arrow-next'),
+    slides = document.getElementsByClassName('slide'),
+    slidingPrev = 0,
+    sliding = 0,
+    transForm
+
+let sliderLength = slides.length
+
+//
+// arrowPrev[0].onclick = function () {
+//     slidingPrev = slidingPrev + 100
+//     transForm = `translateX(${(sliding + slidingPrev)}%)`
+//     for (let slide of slides) {
+//         slide.style.transform = `${transForm}`
+//     }
+//     if (slidingPrev  === 0) {
+//         slidingPrev = sliderLength * (-100)
+//     }
+// }
+// arrowNext[0].onclick = function () {
+//     slidingPrev = slidingPrev - 100
+//     transForm = `translateX(${(sliding + slidingPrev)}%)`
+//     for (let slide of slides) {
+//         slide.style.transform = `${transForm}`
+//     }
+//     if (slidingPrev / sliderLength === 100 || slidingPrev / (sliderLength - 1) === -100) {
+//         slidingPrev = 100
+//     }
+// }
+
+//
+
+
+let onClickPrev = arrowPrev[0].onclick = function slidePrev() {
+    if (slidingPrev === 0) {
+        slidingPrev = sliderLength * (-100)
+    }
+    slidingPrev = slidingPrev + 100
+    transForm = `translateX(${(sliding + slidingPrev)}%)`
+    for (let slide of slides) {
+        slide.style.transform = `${transForm}`
+    }
+    slides[0].style.marginLeft = `${transForm}`
+    console.log(slidingPrev)
+}
+let onClickNext = arrowNext[0].onclick = function slideNext() {
+    if (slidingPrev / (sliderLength - 1) === -100) {
+        slidingPrev = 100
+    }
+    slidingPrev = slidingPrev - 100
+    transForm = `translateX(${(sliding + slidingPrev)}%)`
+    for (let slide of slides) {
+        slide.style.transform = `${transForm}`
+    }
+    // slides[0].style.marginLeft = `${transForm}`
+    console.log(slidingPrev)
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === "ArrowRight") {
+        onClickNext()
+    } else if (event.code === "ArrowLeft") {
+        onClickPrev()
+    }
+})
